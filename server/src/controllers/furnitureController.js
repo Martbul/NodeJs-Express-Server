9b06da0ac2cd6becc64353701553378e85dfa237
Martbul <martbul01@gmail.com>
2023-10-28 22:37:07 +0300

    first commit

---
+++ server/src/controllers/furnitureController.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const furnitureService = require("../services/furnitureService");
+router.get("/", async (req, res) => {
+  try {
+    const furnitures = await furnitureService.getAll();
+    res.json(furnitures);
+  } catch (message) {
+    res.status(400).json({ message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { description, img, make, material, model, price, year } = req.body;
+    await furnitureService.create({
+      description,
+      img,
+      make,
+      material,
+      model,
+      price,
+       year,
+      _ownerId:req.user._id
+    });
+     res.status(201).end()
+  } catch (message) {
+    res.status(400).json({ message });
+  }
+});
+
+module.exports = router;
---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+exports.auth = (req, res, next) => {
+   const token = req.header('X-Authorization')
+
+   if (token) {
+      try {
+        const decodedToken = jwt.verify(token, 'SOME_SECRET');
+        req.user = decodedToken
+        next()
+      } catch (error) {
+        res.status(401).json({ message:'you are not authorized'})
+     }
+   } else {
+     next();
+   }
+}
